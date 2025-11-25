871e54a38a032a9fbc4a6b285ea29ebb3e8f27e2
Jon Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2025-11-25 12:34:58 +0000

    initial nextjs

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+import nextra from "nextra";
+
+// Set up Nextra with its configuration
+const withNextra = nextra({
+  // ... Add Nextra-specific options here
+});
+
+const nextConfig: NextConfig = {
+  turbopack: {
+    resolveAlias: {
+      "next-mdx-import-source-file": "./mdx-components.tsx",
+    },
+  },
+};
+
+export default withNextra(nextConfig);
