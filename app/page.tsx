41925765e41a66eee671f45bcc4572a52dda589f
Jon Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2025-11-25 19:17:03 +0000

    fixing index redirect for static export

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
-import { RedirectType, redirect } from "next/navigation";
+"use client";
+// import { RedirectType, redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("v1", RedirectType.replace);
+  // redirect("v1", RedirectType.replace); // this is the server way, but we need the same effect when statically exported...
+
+  location.replace("v1"); // Make the page a client component in order to use the Browser `location` API.
 }
